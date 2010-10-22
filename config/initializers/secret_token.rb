a8451efcff1529f59fcf2010c1103d463fc65300
Pantsco <ccbrantley@gmail.com>
2010-10-22 11:07:49 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Coop::Application.config.secret_token = '016f8494b89d4dd285f2eb27135a1efc0e92cb2beafa225b33624a3d89d8bb2eb232f4b83a71dba677ffef39c8be6131e7a638569dafc389592f66accffe365f'
